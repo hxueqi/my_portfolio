af2e581b05c8b3d6db598f92f85c9a3589ed35b0
XueqiHuang <hxueqitao@gmail.com>
2023-02-13 18:30:14 +0100

    Finished the footer

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,8 +1,54 @@
 import React from "react";
 import "./footer.css";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { SiCodewars } from "react-icons/si";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer_logo">
+        XUEQI HUANG
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#Exprience">Exprience</a>
+        </li>
+        {/* <li><a href="#Service">Service</a></li> */}
+        <li>
+          <a href="#Portfolio">Portfolio</a>
+        </li>
+        {/* <li><a href="#testimonials">Testimonials</a></li> */}
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://www.linkedin.com/in/xue-qi-huang/">
+          {" "}
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/hxueqi">
+          <FaGithub />
+        </a>
+        <a href="https://www.codewars.com/users/XueqiTao">
+          <SiCodewars />
+        </a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy;Xueqi's personal portfolio. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
